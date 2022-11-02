e3119c8cee0dbc8989c8886d767586496afc24a7
nazar12314 <kononenkonazar47@gmail.com>
2022-11-02 22:05:43 +0200

    Added linting etc.

--- src/test/java/com/example/flowerstore_spring/OrdersTest.java
+++ src/test/java/com/example/flowerstore_spring/OrdersTest.java
@@ -56,9 +56,14 @@ public class OrdersTest {
 
         ordersService.addOrder(order1);
         // Testing getOrders
-        Assertions.assertTrue(ordersService.getOrders().contains(order1));
+        Assertions.assertTrue(
+                ordersService.getOrders().contains(order1)
+        );
         // Testing getOrderById
-        Assertions.assertEquals(ordersService.getOrderById(order1.getId()), order1);
+        Assertions.assertEquals(
+                ordersService.getOrderById(order1.getId()),
+                order1
+        );
     }
 
     @Test
@@ -96,8 +101,8 @@ public class OrdersTest {
         // If order is not processed, it can't be delivered
         Assertions.assertEquals(
                 ordersService.deliverOrder(order.getId()).getMessage(),
-                "Order is not processed," +
-                        " please process your order and pay for it!"
+                "Order is not processed,"
+                        + " please process your order and pay for it!"
         );
 
         // Testing process order
